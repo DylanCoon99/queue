155c90a5ebddd21766512ce1bfbc81daabb24a16
Dylan Coon <Dylan@Mac.lan>
2025-07-30 21:01:30 -0400

    added print functionality

--- queue.go
+++ queue.go
@@ -1,5 +1,6 @@
 package queue
 
+import "errors"
 
 type Node struct {
 	Value    int
@@ -7,25 +8,12 @@ type Node struct {
 	Priority int
 }
 
-
-
-type Queue interface {
-	Insert(params ...int) error  // params is just [val] for normal queue insert (no priority) and [val, priority] for specifc priority
-	Dequeue() int
-}
-
-
-
 type Queue struct {
-	Head *Node    // front of the queue; first to be dequeued
-	Tail *Node    // back of the queue; last to be dequeued
+	Head *Node // front of the queue; first to be dequeued
+	Tail *Node // back of the queue; last to be dequeued
 }
 
-
-
-
-
-func InitNormalQueue() *Queue {
+func InitQueue() *Queue {
 	return &Queue{
 		Head: nil,
 		Tail: nil,
@@ -34,15 +22,80 @@ func InitNormalQueue() *Queue {
 
 
 
+func (q *Queue) PrintQueue() {
 
+	output_string := ""
+
+	cur := q.Tail
+
+	for cur != nil {
+		if cur.Next != nil {
+			output_string += (strconv.Itoa(cur.Value) + " -> ")
+		} else {
+			output_string += (strconv.Itoa(cur.Value))
+		}
+		cur = cur.Next
+	}
+
+	fmt.Println(output_string)
+}
+
+
+
+// Note: 1 is highest priority, higher numbers are lower
 
 func (q *Queue) Insert(params ...int) error {
 	// params is just [val] for normal queue insert (no priority) and [val, priority] for specifc priority
 
 	if len(params) == 2 {
 		// there is a specified priority
+		// iterate over the queue until you find a higher priority node
+		priority := params[1]
+		cur := q.Tail
+
+		node := &Node{
+			Value:    params[0],
+			Next:     nil,
+			Priority: priority,
+		}
+
+		for cur.Next != nil {
+			if cur.Priority > priority {
+				// keep traversing the queue
+				cur = cur.Next
+			} else {
+				temp := cur.Next
+				cur.Next = node
+				node.Next = temp
+				break
+			}
+		}
+
+
+		// have to update the Tail
+
+		return nil
+	} else if len(params) == 1 {
+		// else; just insert to the tail of the queue
+		node := &Node{
+			Value:    params[0],
+			Next:     nil,
+			Priority: 1,
+		}
+
+		if q.Head == nil {
+			q.Tail = node
+			q.Head = node
+			return nil
+		}
+
+		node.Next = q.Tail
+
+
+		q.Tail = node
+		return nil
+	} else {
+		return errors.New("Invalid Params in Insert function")
 	}
 
-
 }
-
--- queue_test.go
+++ queue_test.go
@@ -1,31 +1,40 @@
 package queue
 
 import (
-	"fmt"
 	//"log"
+	"fmt"
 	"testing"
 )
 
-
-
-
+/*
 func InitExampleQueue() {
-
 
 	return
 }
-
-
-
-
+*/
 
 func TestInitQueue(t *testing.T) {
 
+	var queue *Queue
 
+	queue = InitQueue()
+
+	if queue.Head != nil {
+		t.Errorf("InitQueue failed, expected nil got %v", queue.Head)
+	}
+
+	if queue.Tail != nil {
+		t.Errorf("InitQueue failed, expected nil got %v", queue.Tail)
+	}
+
+	fmt.Println("Successfully init queue.")
 
 }
 
+func TestInsert(t *testing.T) {
 
 
+	
 
 
+}
